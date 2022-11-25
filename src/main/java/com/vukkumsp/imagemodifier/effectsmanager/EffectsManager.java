32c6d27503f4bc87f82093ef4f55035e698d4bb5
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 17:33:33 +0530

    image shadowing worked (like whose that pokemon game).

--- src/main/java/com/vukkumsp/imagemodifier/effectsmanager/EffectsManager.java
+++ src/main/java/com/vukkumsp/imagemodifier/effectsmanager/EffectsManager.java
@@ -48,12 +48,23 @@ public class EffectsManager {
         // http://openimaj.org/tutorial/index.html
         MBFImage image = ImageUtilities.readMBFAlpha(new File(this.imagePath));
 
+        
         // ImageUtilities.readMBFAlpha(null)
 
         if(image == null){
             System.out.println("image path is "+ this.imagePath);
             System.out.println("imp is null");
             return;
+        }
+
+        for (int y=0; y<image.getHeight(); y++) {
+        // shade it
+            for(int x=0; x<image.getWidth(); x++) {
+                image.getBand(0).pixels[y][x] = 0; //red
+                image.getBand(1).pixels[y][x] = 0; //blue
+                image.getBand(2).pixels[y][x] = 0; //green
+                
+            }
         }
 
         // for (int y=0; y<image.getHeight(); y++) {
@@ -68,8 +79,8 @@ public class EffectsManager {
         // image.drawShapeFilled(new Ellipse(650f, 425f, 25f, 12f, 0f), RGBColour.WHITE);
         // image.drawShapeFilled(new Ellipse(600f, 380f, 30f, 15f, 0f), RGBColour.WHITE);
         // image.drawShapeFilled(new Ellipse(500f, 300f, 100f, 70f, 0f), RGBColour.WHITE);
-        image.drawText("OpenIMAJ is", 200, 300, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
-        image.drawText("Awesome", 200, 330, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+        // image.drawText("OpenIMAJ is", 200, 300, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+        // image.drawText("Awesome", 200, 330, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
 
         ImageUtilities.write(image, new File(destinationPath));
         
