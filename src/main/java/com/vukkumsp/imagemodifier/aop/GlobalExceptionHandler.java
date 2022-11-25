3db304b9527943dbd278470984e23a22ddc48b1a
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 21:36:32 +0530

    cleanup.

--- src/main/java/com/vukkumsp/imagemodifier/aop/GlobalExceptionHandler.java
+++ src/main/java/com/vukkumsp/imagemodifier/aop/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.vukkumsp.imagemodifier.aop;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
