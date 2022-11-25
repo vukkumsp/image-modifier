217cf9f1e526b85db4e333057f8ec3ce198a10a0
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 17:50:03 +0530

    clean up.

--- src/main/java/com/vukkumsp/imagemodifier/effectsmanager/EffectsManager.java
+++ src/main/java/com/vukkumsp/imagemodifier/effectsmanager/EffectsManager.java
@@ -6,13 +6,6 @@ import java.io.IOException;
 
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
-import org.openimaj.image.colour.RGBColour;
-import org.openimaj.image.typography.hershey.HersheyFont;
-import org.openimaj.math.geometry.shape.Ellipse;
-
-import ij.IJ;
-import ij.ImagePlus;
-import ij.process.ImageProcessor;
 
 public class EffectsManager {
 
@@ -45,22 +38,9 @@ public class EffectsManager {
             }
         }
 
-        // for (int y=0; y<image.getHeight(); y++) {
-        // // Converts image to red filtered
-        //     for(int x=0; x<image.getWidth(); x++) {
-        //         image.getBand(1).pixels[y][x] = 0;
-        //         image.getBand(2).pixels[y][x] = 0;
-        //     }
-        // }
-
-        // image.drawShapeFilled(new Ellipse(700f, 450f, 20f, 10f, 0f), RGBColour.WHITE);
-        // image.drawShapeFilled(new Ellipse(650f, 425f, 25f, 12f, 0f), RGBColour.WHITE);
-        // image.drawShapeFilled(new Ellipse(600f, 380f, 30f, 15f, 0f), RGBColour.WHITE);
-        // image.drawShapeFilled(new Ellipse(500f, 300f, 100f, 70f, 0f), RGBColour.WHITE);
-        // image.drawText("OpenIMAJ is", 200, 300, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
-        // image.drawText("Awesome", 200, 330, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
 
         ImageUtilities.write(image, new File(destinationPath));
+
         
     }
 }
