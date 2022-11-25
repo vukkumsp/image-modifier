680c75048cfb7ce938eea5f3a8d5d90cb6961ee6
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 17:49:34 +0530

    get Image loading to browser done.

--- src/main/java/com/vukkumsp/imagemodifier/rest/MainController.java
+++ src/main/java/com/vukkumsp/imagemodifier/rest/MainController.java
@@ -1,11 +1,18 @@
 package com.vukkumsp.imagemodifier.rest;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vukkumsp.imagemodifier.effectsmanager.EffectsManager;
@@ -36,6 +43,22 @@ class MainController {
     
     return "dummy1";
   }
+
+  //https://www.baeldung.com/spring-controller-return-image-file
+  //https://www.baeldung.com/convert-file-to-input-stream
+  @GetMapping(
+  value = "/shadeIt",
+  produces = MediaType.IMAGE_PNG_VALUE)
+  public @ResponseBody byte[] getImage() throws IOException {
+    EffectsManager em = new EffectsManager(LocalFileManager.sampleImageUrl2);
+
+    em.applyDemoEffect2("C://Users//vukku//Documents//ImageDestination//im2.png");
+    
+    File initialFile = new File("C://Users//vukku//Documents//ImageDestination//im2.png");
+    InputStream targetStream = new FileInputStream(initialFile);
+    
+    return IOUtils.toByteArray(targetStream);
+}
 
   @GetMapping("/throwException")
   String throwException(){
