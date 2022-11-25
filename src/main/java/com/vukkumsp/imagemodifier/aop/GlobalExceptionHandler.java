e1ec194bb3fff237e96a1921597974e783f6ee9f
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 21:38:38 +0530

    cleanup.

--- src/main/java/com/vukkumsp/imagemodifier/aop/GlobalExceptionHandler.java
+++ src/main/java/com/vukkumsp/imagemodifier/aop/GlobalExceptionHandler.java
@@ -1,29 +1,29 @@
-package com.vukkumsp.imagemodifier.aop;
+// package com.vukkumsp.imagemodifier.aop;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
+// import org.aspectj.lang.ProceedingJoinPoint;
+// import org.aspectj.lang.annotation.Around;
+// import org.aspectj.lang.annotation.Aspect;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.context.annotation.Configuration;
 
-import com.vukkumsp.imagemodifier.exceptions.GlobalException;
+// import com.vukkumsp.imagemodifier.exceptions.GlobalException;
 
-@Aspect
-@Configuration
-public class GlobalExceptionHandler {
+// @Aspect
+// @Configuration
+// public class GlobalExceptionHandler {
     
-    private Logger logger = LoggerFactory.getLogger(RestApiLogger.class);
+//     private Logger logger = LoggerFactory.getLogger(RestApiLogger.class);
 
-    @Around("execution(* com.vukkumsp.imagemodifier.rest.*.*(..))")
-    public Object handler(ProceedingJoinPoint joinPoint) throws Throwable{
-        try{
-            Object obj = joinPoint.proceed();
-            return obj;
-        }
-        catch(GlobalException ex){
-            logger.info("Global Exception occurred");
-        }
-        return "throwException"; //Handle it well
-    }
-}
+//     @Around("execution(* com.vukkumsp.imagemodifier.rest.*.*(..))")
+//     public Object handler(ProceedingJoinPoint joinPoint) throws Throwable{
+//         try{
+//             Object obj = joinPoint.proceed();
+//             return obj;
+//         }
+//         catch(GlobalException ex){
+//             logger.info("Global Exception occurred");
+//         }
+//         return "throwException"; //Handle it well
+//     }
+// }
