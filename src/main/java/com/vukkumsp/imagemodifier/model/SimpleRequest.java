8533f48bf6a88e027bc4d884a0418983286864eb
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2022-11-25 18:04:38 +0530

    post method to send local image location as request object and received modified image as expected.

---
+++ src/main/java/com/vukkumsp/imagemodifier/model/SimpleRequest.java
@@ -0,0 +1,14 @@
+package com.vukkumsp.imagemodifier.model;
+
+public class SimpleRequest {
+    private String imageSourcePath;
+
+    public String getImageSourcePath() {
+        return this.imageSourcePath;
+    }
+
+    public void setImageSourcePath(String imageSourcePath) {
+        this.imageSourcePath = imageSourcePath;
+    }
+
+}
--- src/main/java/com/vukkumsp/imagemodifier/rest/MainController.java
+++ src/main/java/com/vukkumsp/imagemodifier/rest/MainController.java
@@ -12,12 +12,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vukkumsp.imagemodifier.effectsmanager.EffectsManager;
 import com.vukkumsp.imagemodifier.exceptions.GlobalException;
 import com.vukkumsp.imagemodifier.filemanager.LocalFileManager;
+import com.vukkumsp.imagemodifier.model.SimpleRequest;
 
 @RestController
 class MainController {
@@ -40,28 +42,39 @@ class MainController {
     EffectsManager em = new EffectsManager(LocalFileManager.sampleImageUrl2);
 
     em.applyDemoEffect2("C://Users//vukku//Documents//ImageDestination//im2.png");
-    
+
     return "dummy1";
   }
 
-  //https://www.baeldung.com/spring-controller-return-image-file
-  //https://www.baeldung.com/convert-file-to-input-stream
-  @GetMapping(
-  value = "/shadeIt",
-  produces = MediaType.IMAGE_PNG_VALUE)
+  // https://www.baeldung.com/spring-controller-return-image-file
+  // https://www.baeldung.com/convert-file-to-input-stream
+  @GetMapping(value = "/shadeIt", produces = MediaType.IMAGE_PNG_VALUE)
   public @ResponseBody byte[] getImage() throws IOException {
     EffectsManager em = new EffectsManager(LocalFileManager.sampleImageUrl2);
 
     em.applyDemoEffect2("C://Users//vukku//Documents//ImageDestination//im2.png");
-    
+
     File initialFile = new File("C://Users//vukku//Documents//ImageDestination//im2.png");
     InputStream targetStream = new FileInputStream(initialFile);
-    
+
     return IOUtils.toByteArray(targetStream);
-}
+  }
+
+  @PostMapping(value = "/shadeIt2", produces = MediaType.IMAGE_PNG_VALUE)
+  public @ResponseBody byte[] postImage(@RequestBody SimpleRequest simpleRequest) throws IOException {
+    
+    EffectsManager em = new EffectsManager(simpleRequest.getImageSourcePath());
+
+    em.applyDemoEffect2("C://Users//vukku//Documents//ImageDestination//im2.png");
+
+    File initialFile = new File("C://Users//vukku//Documents//ImageDestination//im2.png");
+    InputStream targetStream = new FileInputStream(initialFile);
+
+    return IOUtils.toByteArray(targetStream);
+  }
 
   @GetMapping("/throwException")
-  String throwException(){
+  String throwException() {
     throw new GlobalException("failed");
   }
 }
